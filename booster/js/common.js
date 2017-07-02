149885eb93e6ebdb0f97eb72328bc4d714af550f
BonoBelarus <bono.ex@gmail.com>
2017-07-02 17:51:03 +0300

    1

--- booster/js/common.js
+++ booster/js/common.js
@@ -3,8 +3,15 @@ document.addEventListener('DOMContentLoaded', function(){
 		offset_top: -50
 	});
 
+	$('.to-up').click(function(){
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
 	$('.country-label-content .items').slick({
 		nextArrow: '<div class="slick-next"></div>',
+		prevArrow: '<div class="slick-prev"></div>',
 		dots: true,
 		autoplay: true,
 		autoplaySpeed: 5000
@@ -42,30 +49,10 @@ document.addEventListener('DOMContentLoaded', function(){
 		var section = $(link);
 
 		var xtra_offset = 0;
-		switch(link){
-			case '#where-2-buy':
-			case '#about':
-			case '#products':
-				if($(window).width() > 1024){
-					xtra_offset = 80; break;
-				}else{
-					xtra_offset = -60; break;
-				}
-			case '#technology':
-				if($(window).width() > 1024){
-					xtra_offset = 80; break;
-					toCountNumbers('#technology');
-				}else{
-					toCountNumbers('#technology');
-					xtra_offset = -60; break;
-				}
-			break;
-		}
 		
 		$('body,html').animate({
 			scrollTop: $(section).offset().top + xtra_offset
 		}, 1000);
-		animateNav(e)
 		e.preventDefault();
 	}
 	$('nav a').click(scroll2Sec);
