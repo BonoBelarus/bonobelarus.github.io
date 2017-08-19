8de1dc16e49f1b0f32909a7da621b505e747f067
bono <bono.ex@gmail.com>
2017-08-20 02:15:09 +0300

    1

--- test/js/common.js
+++ test/js/common.js
@@ -1,14 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(){
-  var newsContent = document.querySelectorAll('.news-brief .txt');
-  var size = 250
+document.addEventListener("DOMContentLoaded", function(){
+  var newsContent = document.querySelectorAll(".news-brief .txt");
+  var size = 250;
 
   for(var i = 0; i < newsContent.length; i++){
     var newsText = newsContent[i].innerText;
     
     if(newsText.length > size){
-      newsContent[i].innerText = newsText.slice(0, size) + ' ...'
+      newsContent[i].innerText = newsText.slice(0, size) + " ...";
     }
   }
-  
-  
+
 });
